a308baad6a0d40707ba05bd3f49e5edcb0e4c092
nairbdev2 <bdevil2@uic.edu>
2020-02-06 21:21:54 -0600

    Adjusted Player code

--- Multiplayer Assignment/Assets/Scripts/PlayerMove.cs
+++ Multiplayer Assignment/Assets/Scripts/PlayerMove.cs
@@ -7,23 +7,21 @@ public class PlayerMove : NetworkBehaviour
 {
 
     //Array for Mesh Renderer of Children
-    public Component[] Renderer;
+    private Component[] Renderer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!isLocalPlayer)
-            return;
+    float speed = 25.0f;
+    float rotationSpeed = 90;
+    float force = 200f;
 
-        var x = Input.GetAxis("Horizontal") * 0.1f;
-        var z = Input.GetAxis("Vertical") * 0.1f;
+    Rigidbody rb;
+    Transform t;
 
-        transform.Translate(x, 0, z);
-    }
-
-    
+    //Initializing (Replaces Start())
     public override void OnStartLocalPlayer()
     {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent <Transform>();
+
 
         Renderer[] rs = GetComponentsInChildren<Renderer>();
 
@@ -33,11 +31,45 @@ public class PlayerMove : NetworkBehaviour
             r.material.color = Color.red;
         }
 
+    }
 
 
-        //GetComponent<MeshRenderer>().material.color = Color.red;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        // ----------------------------------------------------
+        //                  Basic Movement Set
+        // ----------------------------------------------------
+        if (Input.GetKey(KeyCode.W))
+            rb.velocity += this.transform.forward * speed * Time.deltaTime;                 // Move Forwards
+        else if (Input.GetKey(KeyCode.S))                                                   // Move Backwards
+            rb.velocity -= this.transform.forward * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            t.rotation *= Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0);           // Rotate Right
+        else if (Input.GetKey(KeyCode.A))
+            t.rotation *= Quaternion.Euler(0, -rotationSpeed * Time.deltaTime, 0);          // Rotate Left
+
+        if (Input.GetKeyDown(KeyCode.Space))                                                // Increase Altitude
+            rb.AddForce(t.up * force);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))                                            // Lower Altitude
+            rb.AddForce(t.up * -force);
+
+        // ----------------------------------------------------
+        //                  Interact Control
+        // ----------------------------------------------------
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Implement Something to pick up object
+            Debug.Log("Claiming Nest~!");
+        }
+
 
 
     }
-    
 }
