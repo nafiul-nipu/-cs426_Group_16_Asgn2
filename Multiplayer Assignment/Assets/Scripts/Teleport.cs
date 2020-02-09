fa4c751fae97913953bf87cecdebc07c2299d05c
JGuo99 <54557461+JGuo99@users.noreply.github.com>
2020-02-08 20:29:46 -0600

    Added collsions to PC and teleport script.

---
+++ Multiplayer Assignment/Assets/Scripts/Teleport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform Dest = null; //Destination Player will be teleported to.
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //When the object is allowed to teleport
+        if (other.gameObject.tag == "Teleport") //Tag object to activate teleportation
+            this.transform.position = Dest.position;
+    }
+}
